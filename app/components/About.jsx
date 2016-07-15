f1646b5e6f16751882a73584ba1871bf8c10b1ed
Andrew Warko <andrewwarko@users.noreply.github.com>
2016-07-14 20:45:26 -0400

    Style about and Examples page, and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,17 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the About page.</p>
+            <h1 className="text-center">About</h1>
+            <p>This is a weather application built on React for learning purposes.</p>
+            <p>Some of the tools used in this project:</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map</a>
+                </li>
+            </ul>
         </div>
 
     );
